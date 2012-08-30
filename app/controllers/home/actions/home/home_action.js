669d64b20d54845288b0e4263aab1c6f17246f02
Dave Edelhart <dave@wonderlandlabs.com>
2012-08-30 12:50:18 -0700

    adding layout component; initial work on layout

--- app/controllers/home/actions/home/home_action.js
+++ app/controllers/home/actions/home/home_action.js
@@ -1,20 +1,70 @@
 var util = require('util');
-var _DEBUG = false;
+var _DEBUG = true;
 module.exports = {
-    on_validate: function(rs){
+    on_validate:function (rs) {
         this.on_input(rs);
     },
 
-    on_input: function(rs){
+    on_input:function (rs) {
         this.on_process(rs, rs.req_props);
     },
 
-    on_process: function(rs, input){
-        if (!input){
-            input = {name: 'World'}
-        } else if (!input.name){
+    on_process:function (rs, input) {
+        if (!input) {
+            input = {name:'World'}
+        } else if (!input.name) {
             input.name = 'World';
         }
+        /* ************* SIDEBER ****** */
+
+
+        if (input.sidebar) {
+            input.sidebar = [
+                {  title:'Sidebar title',
+                    links:[
+                        {link:'/section/1', title:'Section One'},
+                        {link:'/section/2', title:'Section Two'}
+                    ]}
+            ];
+        }
+        /* ************* NAV ********** */
+
+        if (input.nav) {
+            input.nav = [
+                {
+                    dropdown:true,
+                    title:'Sections',
+                    links:[
+                        {link:'/section/1', title:'Section One'},
+                        {link:'/section/2', title:'Section Two'}
+                    ]
+                },
+                {
+                    title:'Section 3', link:'/section/3'
+                }
+
+            ]
+        }
+
+        /* *********** HERO ************* */
+
+        if (input.hero) {
+            input.hero = {
+                more:{
+                    title:'Xena Warrior Princess',
+                    link:'/xena'
+                },
+                title:'I need a hero',
+                text:'Where have all good men gone <br />' +
+                    'And where are all the gods? <br />' +
+                    'Where’s the street-wise Hercules <br />' +
+                    'To fight the rising odds? <br />' +
+                    ' <br />' +
+                    'Isn’t there a white knight upon a fiery steed? <br />' +
+                    'Late at night I toss and turn and dream of what I need'
+            }
+        }
+
         if (_DEBUG) console.log('outputting %s', util.inspect(input))
         this.on_output(rs, input);
     }
