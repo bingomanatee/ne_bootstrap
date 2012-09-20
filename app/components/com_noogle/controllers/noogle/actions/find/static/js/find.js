984371a0d5996c70bd13c58f3331ed0a0b875cd1
Dave Edelhart <dave@wonderlandlabs.com>
2012-09-20 08:07:27 -0700

    good user, word filtering

--- app/components/com_noogle/controllers/noogle/actions/find/static/js/find.js
+++ app/components/com_noogle/controllers/noogle/actions/find/static/js/find.js
@@ -27,6 +27,8 @@ function MessagesCtrl($scope, $filter, $compile, Messages) {
     $scope.start = 0;
     $scope.show_words = true;
     $scope.words = [];
+
+    $scope.users = [];
     
     var _stopwords = 'a,about,above,after,again,against,all,am,an,and,any,are,aren\'t,as,at,be,because,been,before,being,below,between,both,but,by,can\'t,cannot,could,couldn\'t,did,didn\'t,do,does,doesn\'t,doing,don\'t,down,during,each,few,for,from,further,had,hadn\'t,has,hasn\'t,have,haven\'t,having,he,he\'d,he\'ll,he\'s,her,here,here\'s,hers,herself,him,himself,his,how,how\'s,i,i\'d,i\'ll,i\'m,i\'ve,if,in,into,is,isn\'t,it,it\'s,its,itself,let\'s,me,more,most,mustn\'t,my,myself,no,nor,not,of,off,on,once,only,or,other,ought,our,ours,ourselves,out,over,own,same,shan\'t,she,she\'d,she\'ll,she\'s,should,shouldn\'t,so,some,such,than,that,that\'s,the,their,theirs,them,themselves,then,there,there\'s,these,they,they\'d,they\'ll,they\'re,they\'ve,this,those,through,to,too,under,until,up,very,was,wasn\'t,we,we\'d,we\'ll,we\'re,we\'ve,were,weren\'t,what,what\'s,when,when\'s,where,where\'s,which,while,who,who\'s,whom,why,why\'s,with,won\'t,would,wouldn\'t,you,you\'d,you\'ll,you\'re,you\'ve,your,yours,yourself,yourselves'.split(',')
 
@@ -41,32 +43,43 @@ function MessagesCtrl($scope, $filter, $compile, Messages) {
         }
     }
 
-    $scope.$watch('noogle_tab', function(t){
-        console.log(t);
-        /**
-         * function(){
-         $('#word_list').isotope({
-         // options
-         itemSelector : 'span',
-         layoutMode : 'fitRows'
-         });
-         }
-         */
-    })
+    function _is_weighted(word){   return word.weighted
+    }
+    $scope.weighted_words = function(){
+        var ww = {words: _.pluck( _.filter($scope.words, _is_weighted), 'word')};
+
+        ww.username = _.pluck( _.filter($scope.users, _is_weighted), 'username');
+        return ww;
+    }
 
     $scope.$watch('messages', function(messages){
 
         if (messages&& messages.length){
             var words = _.reduce($scope.messages, function(w, m){
                 var mw = m.message.toLowerCase().split(/[\W]+/i);
-                w = _.sortBy(_.uniq(w.concat(mw)), _.identity);
-
+                w = _.union(mw, w);
+                m.words = mw;
                 return w;
             }, []);
+
+            words = _.sortBy(words, _.identity);
 
             $scope.words = _.map(words, function(w){
                 return {word: w, weighted: false, stop:_.include(_stopwords, w)};
             })
+
+            var users = _.reduce($scope.messages, function(u, m){
+               if (!(_.include(u, m.username))){
+                   u.push(m.username);
+               }
+                return u;
+            }, []);
+
+            users = _.sortBy(users, _.identity);
+
+            $scope.users = _.map(users, function(u){
+               return {username: u, weighted: false};
+            });
 
         } else {
             $scope.words = [];
